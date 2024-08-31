72a5791d4c5a623bb70dc7b88d6f715b6626ba43
tendaikawadza2 <zimjsc2020@gmail.com>
2024-08-31 13:20:21 +0200

    addec domain to cors

--- src/main/java/io/getarrays/securecapita/SecureCapitaApplication.java
+++ src/main/java/io/getarrays/securecapita/SecureCapitaApplication.java
@@ -42,7 +42,7 @@ public class SecureCapitaApplication {
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:3000", "http://securecapita.org"));
+		corsConfiguration.setAllowedOrigins(List.of("https://processmanagement.tanawellnesscoaching.com","http://localhost:4200", "http://localhost:3000", "http://securecapita.org"));
 		//corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Jwt-Token", "Authorization", "Origin", "Accept", "X-Requested-With",
