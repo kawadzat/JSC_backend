43d72c4356b694c6c1a3e822e35e4d126a8b9f18
tendaikawadza2 <zimjsc2020@gmail.com>
2024-10-24 21:20:31 +0200

    Revert "Merge branch 'public' into main"
    This reverts commit ba1a7a107471adf74725a7c9d8dfe43a43134d42, reversing
    changes made to 5e66a746a03b8357f02e9295b81e48046d7411c5.

--- src/main/java/io/getarrays/securecapita/SecureCapitaApplication.java
+++ src/main/java/io/getarrays/securecapita/SecureCapitaApplication.java
@@ -43,7 +43,8 @@ public class SecureCapitaApplication {
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(List.of("https://processmanagement.tanawellnesscoaching.com","https://processafr.com","http://172.18.5.36:9092","http://localhost:4200", "http://localhost:3000", "http://securecapita.org"));
+		corsConfiguration.setAllowedOrigins(List.of("https://processmanagement.tanawellnesscoaching.com","http://localhost:4200","http://172.18.5.36:9092", "http://localhost:3000", "http://securecapita.org"));
+		//corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Jwt-Token", "Authorization", "Origin", "Accept", "X-Requested-With",
 				"Access-Contro" +
